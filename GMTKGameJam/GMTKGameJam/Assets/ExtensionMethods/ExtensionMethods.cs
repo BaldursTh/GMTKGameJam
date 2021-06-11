aef56555c4f45ab88e4fbc6639e17fd9cf4ee123
BaldursTh <edsimonds654@gmail.com>
2021-06-12 08:01:50 +1000

    Player Shooting

---
+++ GMTKGameJam/GMTKGameJam/Assets/ExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+    public static Vector2 GetMouseDirection(this Transform transform)
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        Vector2 direction = (mousePos - transform.position).normalized;
+        return direction;
+    }
+    public static float GetMouseAngle(this Transform transform)
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        Vector2 direction = (mousePos - transform.position).normalized;
+        float mouseAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        return mouseAngle;
+    }
+
+}
