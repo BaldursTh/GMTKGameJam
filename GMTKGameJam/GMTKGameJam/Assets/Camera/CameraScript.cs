6e71e1a7377691d6b071c6a5ac34b722d74210b8
woofymax <64120270+woofymax@users.noreply.github.com>
2021-06-13 17:03:05 +1000

    fade and death

--- GMTKGameJam/GMTKGameJam/Assets/Camera/CameraScript.cs
+++ GMTKGameJam/GMTKGameJam/Assets/Camera/CameraScript.cs
@@ -1,22 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraScript : MonoBehaviour
 {
     public GameObject player;
 
-   float posX;
-       float  posY;
+
+    int isDead = 0;
+    int showDeathScreen = 0;
+    float posX;
+    float  posY;
     Vector2 position;
     void Update()
     {
-        position = player.transform.position;
-        posX = position.x;
-        posY = position.y;
+        if(isDead == 0)
+        {
+            position = player.transform.position;
+            posX = position.x;
+            posY = position.y;
 
-        posX = Mathf.Round(posX / 0.0625f) * 0.0625f;
-        posY = Mathf.Round(posY / 0.0625f) * 0.0625f;
-        transform.position = new Vector3(posX, posY, -10); 
+            posX = Mathf.Round(posX / 0.0625f) * 0.0625f;
+            posY = Mathf.Round(posY / 0.0625f) * 0.0625f;
+            transform.position = new Vector3(posX, posY, -10);
+        } else if(isDead == 1)
+        {
+            Death();
+        }
+        
     }
+
+    public void Kill()
+    {
+        isDead = 1;
+    }
+
+    IEnumerator Death()
+    {
+        showDeathScreen++;
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("CameraPan", LoadSceneMode.Single);
+    }
+
+
 }
